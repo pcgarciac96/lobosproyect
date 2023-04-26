c8014346d3dbdf67c8fd5bf21d82aac09afe1317
pcgarciac96 <garcuapunk@gmail.com>
2023-04-26 16:05:12 -0500

    corrige error

--- frontend/src/views/user/UserRegister.vue
+++ frontend/src/views/user/UserRegister.vue
@@ -77,6 +77,7 @@ export default {
       console.log(user);
       userRegister(user)
         .then((res) => {
+          console.log(res)
           router.push({ name: "Profile" });
         })
         .catch((error) => console.log(error));
